9fbd94fee5098a38ca947f99e53f19bb59e3ce7f
Amit Wani <mtwn105@gmail.com>
2025-03-18 20:09:47 +0530

    Initial commit

---
+++ src/main/java/dev/amitwani/mcp_spring_java/McpSpringJavaApplication.java
@@ -0,0 +1,13 @@
+package dev.amitwani.mcp_spring_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class McpSpringJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(McpSpringJavaApplication.class, args);
+	}
+
+}
